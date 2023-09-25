e62a037d7dc1d3cdaebb4036ae746f4100cd5843
@hexlet/cli <support@hexlet.io>
2023-09-25 21:08:00 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -1,22 +1,21 @@
 package exercise;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 // BEGIN
 public class App {
+    public static final String[][] EMPTY_ARRAYS = {};
     public static String[][] enlargeArrayImage(String[][] image) {
-        var newYLength = image.length * 2;
-        var newXLength = image[0].length * 2;
-        var resultArray = new String[newYLength][newXLength];
-
-        for (int i = 0, y = 0; i < resultArray.length; i+=2, y++) {
-            for (int j = 0, x = 0; j < resultArray[i].length; j+=2, x++) {
-                var copyValue = image[y][x];
-                resultArray[i][j] = copyValue;
-                resultArray[i + 1][j] = copyValue;
-                resultArray[i][j + 1] = copyValue;
-                resultArray[i + 1][j + 1] = copyValue;
-            }
+        if(image.length == 0) {
+            return EMPTY_ARRAYS;
         }
-        return resultArray;
+        return Arrays.stream(image)
+                .map(array -> Arrays.stream(array)
+                        .flatMap(item -> Stream.of(item,item))
+                        .toArray(String[]::new))
+                .flatMap(array -> Stream.of(array, array))
+                .toArray(String[][]::new);
     }
 }
 // END
--- java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -8,6 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 // BEGIN
 public class AppTest {
     private static String[][] image;
+    private static String[][] emptyImage;
+    private static String[][] oneElementImage;
     @BeforeAll
     public static void init() {
         image = new String[][] {
@@ -16,15 +18,29 @@ public class AppTest {
                 {"*", " ", " ", "*"},
                 {"*", "*", "*", "*"},
         };
+        emptyImage = new String[][] {};
+        oneElementImage = new String[][] {{"*"}};
+
     }
 
     @Test
     public void check_new_length_enlargeArrayImage() {
-        var newArray = App.enlargeArrayImage(image);
+        var expectedArray = App.enlargeArrayImage(image);
         var expectedYSize = image.length * 2;
         var expectedXSize = image.length * 2;
-        assertThat(newArray.length).isEqualTo(expectedYSize);
-        assertThat(newArray[0].length).isEqualTo(expectedXSize);
+        assertThat(expectedArray.length).isEqualTo(expectedYSize);
+        assertThat(expectedArray[0].length).isEqualTo(expectedXSize);
+
+        expectedArray = App.enlargeArrayImage(emptyImage);
+        expectedYSize = 0;
+        assertThat(expectedArray.length).isEqualTo(expectedYSize);
+
+        expectedArray = App.enlargeArrayImage(oneElementImage);
+        expectedXSize = 2;
+        expectedYSize = 2;
+        assertThat(expectedArray.length).isEqualTo(expectedYSize);
+        assertThat(expectedArray[0].length).isEqualTo(expectedXSize);
+
     }
 
     @Test
@@ -42,6 +58,20 @@ public class AppTest {
         };
 
         assertThat(expectedArray).isEqualTo(actualArray);
+
+        actualArray = App.enlargeArrayImage(emptyImage);
+        expectedArray = new String[][] {};
+
+        assertThat(expectedArray).isEqualTo(actualArray);
+
+        actualArray = App.enlargeArrayImage(oneElementImage);
+        expectedArray = new String[][] {
+                {"*", "*"},
+                {"*", "*"}
+        };
+
+        assertThat(expectedArray).isEqualTo(actualArray);
+
     }
 }
 // END
